5a7b095b1cd6c34f6277d9de6f2488d8bbcac12b
Lucy Wanzer <lwanzer@wellesley.edu>
2018-09-18 19:35:35 -0400

    working code with year 4 data

--- Lucy/HYPM_Code.m
+++ Lucy/HYPM_Code.m
@@ -109,6 +109,46 @@ filename = ['deployment0003_GI02HYPM-WFP02-01-FLORDL000-recovered_wfp-flord_l_wf
         %the backscatter or scat_total results
     %Convert to matlab time
     Yr3_wfp.time_flord_mat = convertTime(Yr1_wfp.time_flord);
+    
+    %% Extract Year 4 Data
+%Wire-following profiler, Year 4, DOSTA    
+filename = ['deployment0004_GI02HYPM-WFP02-03-DOSTAL000-recovered_wfp-dosta_ln_wfp_instrument_recovered_20170807T000204-20180615T185737.nc']; ncdisp(filename)
+    Yr4_wfp.time_dosta = ncread(filename,'time');
+    Yr4_wfp.lon_dosta = ncread(filename,'lon');
+    Yr4_wfp.lat_dosta = ncread(filename,'lat');
+    %CTD data - Note that these appear to be directly taken from corresponding points in
+    %CTD file, and so could be used without having to pull from the CTD data
+    Yr4_wfp.temperature_dosta = ncread(filename,'ctdpf_ckl_seawater_temperature'); %standard_name = 'sea_water_temperature' units = 'deg_C'
+    Yr4_wfp.pracsal_dosta = ncread(filename,'practical_salinity'); %standard_name = 'sea_water_practical_salinity'
+    Yr4_wfp.pressure_dosta = ncread(filename,'int_ctd_pressure'); %standard_name = 'sea_water_pressure' units = 'dbar'
+    %Optode data
+    Yr4_wfp.oxygen = ncread(filename,'dissolved_oxygen'); %standard_name = 'moles_of_oxygen_per_unit_mass_in_sea_water' units = 'umol kg-1'
+    Yr4_wfp.optode_temperature = ncread(filename,'optode_temperature'); %long_name = 'Optode Temperature' units = 'deg_C'
+        %Note that these points fall close to, but not exactly on, a 1:1
+        %line with the CTD temperature. Points with zero value of optode
+        %temperature may be indicator of bad oxygen data points.
+    %Convert to matlab time
+    Yr4_wfp.time_dosta_mat = convertTime(Yr1_wfp.time_dosta);
+
+%Wire-following profiler, Year 4, Fluorometer    
+filename = ['deployment0004_GI02HYPM-WFP02-01-FLORDL000-recovered_wfp-flord_l_wfp_instrument_recovered_20170807T000204-20180615T185737.nc']; ncdisp(filename)
+    Yr4_wfp.time_flord = ncread(filename,'time');
+    Yr4_wfp.lon_flord = ncread(filename,'lon');
+    Yr4_wfp.lat_flord = ncread(filename,'lat');
+    %CTD data - Note that these appear to be directly taken from corresponding points in
+    %CTD file, and so could be used without having to pull from the CTD data
+    Yr4_wfp.temperature_flord = ncread(filename,'ctdpf_ckl_seawater_temperature'); %standard_name = 'sea_water_temperature' units = 'deg_C'
+    Yr4_wfp.pracsal_flord = ncread(filename,'practical_salinity'); %standard_name = 'sea_water_practical_salinity'
+    Yr4_wfp.pressure_flord = ncread(filename,'int_ctd_pressure'); %standard_name = 'sea_water_pressure' units = 'dbar'
+    %Fluorometer data
+    Yr4_wfp.backscatter = ncread(filename,'optical_backscatter'); %long_name = 'Optical Backscatter' units = 'm-1'
+    Yr4_wfp.scat_total = ncread(filename,'seawater_scattering_coefficient'); %long_name = 'Total Scattering Coefficient of Pure Seawater' units = 'm-1'
+    Yr4_wfp.chla = ncread(filename,'fluorometric_chlorophyll_a'); %long_name = 'Chlorophyll-a Concentration' units = 'ug L-1'
+        %Note that there are other scattering based results that I don't
+        %really understand - look back at these if I actually want to use
+        %the backscatter or scat_total results
+    %Convert to matlab time
+    Yr4_wfp.time_flord_mat = convertTime(Yr1_wfp.time_flord);
 
  %% Assign profile indices prior to gridding
 Yr1_wfp.depth_dosta = -gsw_z_from_p(Yr1_wfp.pressure_dosta,Yr1_wfp.lat_dosta);
@@ -119,11 +159,15 @@ Yr2_wfp.depth_dosta = -gsw_z_from_p(Yr2_wfp.pressure_dosta,Yr2_wfp.lat_dosta);
     
 Yr3_wfp.depth_dosta = -gsw_z_from_p(Yr3_wfp.pressure_dosta,Yr3_wfp.lat_dosta);
     [Yr3_wfp.profile_index,Yr3_wfp.updown_index] = profileIndex(Yr3_wfp.depth_dosta);
+    
+Yr4_wfp.depth_dosta = -gsw_z_from_p(Yr4_wfp.pressure_dosta,Yr4_wfp.lat_dosta);
+    [Yr4_wfp.profile_index,Yr4_wfp.updown_index] = profileIndex(Yr4_wfp.depth_dosta)
 
 %% Calculate density in raw profiles to enable gridding on density surfaces
 Yr1_wfp.pdens = gsw_p_from_z(-Yr1_wfp.depth_dosta, Yr1_wfp.lat_dosta); %potential density function
 Yr2_wfp.pdens = gsw_p_from_z(-Yr2_wfp.depth_dosta, Yr2_wfp.lat_dosta);
 Yr3_wfp.pdens = gsw_p_from_z(-Yr3_wfp.depth_dosta, Yr3_wfp.lat_dosta);
+Yr4_wfp.pdens = gsw_p_from_z(-Yr4_wfp.depth_dosta, Yr4_wfp.lat_dosta);
 
 %% Grid data to consistent depth intervals for each profile
 depth_grid = [150:5:2600];
@@ -156,11 +200,20 @@ Yr3_wfpgrid.time_start = convertTime(Yr3_wfpgrid.time_start);
 Yr3_wfpgrid.duration = Yr3_wfpgrid.duration/secinday;
 Yr3_wfpgrid.updown = Yr3_wfpgrid.profile_direction; 
 
+%All profiles for year 3
+scivars = [Yr4_wfp.temperature_dosta, Yr4_wfp.pracsal_dosta, Yr4_wfp.oxygen, Yr4_wfp.optode_temperature...
+        Yr4_wfp.backscatter, Yr4_wfp.scat_total, Yr4_wfp.chla];
+[Yr4_wfpgrid] = glider_grid(Yr4_wfp.time_dosta,Yr4_wfp.lat_dosta,Yr4_wfp.lon_dosta,Yr4_wfp.depth_dosta,Yr4_wfp.profile_index,Yr4_wfp.updown_index',scivars,depth_grid);
+    Yr4_wfpgrid.depth_grid = depth_grid;
+Yr4_wfpgrid.time_start = convertTime(Yr4_wfpgrid.time_start);
+Yr4_wfpgrid.duration = Yr4_wfpgrid.duration/secinday;
+Yr4_wfpgrid.updown = Yr4_wfpgrid.profile_direction; 
 %% Take mean of paired up and down profiles
     tol = 1; %only combine profiles where time_start is < 1 day apart
 [Yr1_wfpgrid.scivars_pair,Yr1_wfpgrid.ind_pair] = profilePairMean(Yr1_wfpgrid,tol);
 [Yr2_wfpgrid.scivars_pair,Yr2_wfpgrid.ind_pair] = profilePairMean(Yr2_wfpgrid,tol);
 [Yr3_wfpgrid.scivars_pair,Yr3_wfpgrid.ind_pair] = profilePairMean(Yr3_wfpgrid,tol);
+[Yr4_wfpgrid.scivars_pair,Yr4_wfpgrid.ind_pair] = profilePairMean(Yr4_wfpgrid,tol);
 
 %% Unpack scivars in gridded form
 %When using scivars, gets all profiles (both up and down)
@@ -191,7 +244,7 @@ Yr2_wfpgrid.pdens = gsw_sigma0(Yr2_wfpgrid.S,Yr2_wfpgrid.T)+1000;
 Yr2_wfpgrid.press = gsw_p_from_z(repmat(-Yr2_wfpgrid.depth_grid,length(Yr2_wfpgrid.profile_ind),1)',...
         repmat(Yr2_wfpgrid.lat,1,length(Yr2_wfpgrid.depth_grid))');      
     
-    % Year 3
+         % Year 3
 Yr3_wfpgrid.T = squeeze(Yr3_wfpgrid.scivars_pair(:,1,:));
 Yr3_wfpgrid.S = squeeze(Yr3_wfpgrid.scivars_pair(:,2,:));
 Yr3_wfpgrid.O2conc = squeeze(Yr3_wfpgrid.scivars_pair(:,3,:));
@@ -203,6 +256,19 @@ Yr3_wfpgrid.pdens = gsw_sigma0(Yr3_wfpgrid.S,Yr3_wfpgrid.T)+1000;
 Yr3_wfpgrid.press = gsw_p_from_z(repmat(-Yr3_wfpgrid.depth_grid,length(Yr3_wfpgrid.profile_ind),1)',...
         repmat(Yr3_wfpgrid.lat,1,length(Yr3_wfpgrid.depth_grid))'); 
     
+    % Year 4
+Yr4_wfpgrid.T = squeeze(Yr4_wfpgrid.scivars_pair(:,1,:));
+Yr4_wfpgrid.S = squeeze(Yr4_wfpgrid.scivars_pair(:,2,:));
+Yr4_wfpgrid.O2conc = squeeze(Yr4_wfpgrid.scivars_pair(:,3,:));
+Yr4_wfpgrid.optode_temperature = squeeze(Yr4_wfpgrid.scivars_pair(:,4,:));
+Yr4_wfpgrid.backscatter = squeeze(Yr4_wfpgrid.scivars_pair(:,5,:));
+Yr4_wfpgrid.scat_total = squeeze(Yr4_wfpgrid.scivars_pair(:,6,:));
+Yr4_wfpgrid.chla = squeeze(Yr4_wfpgrid.scivars_pair(:,7,:));
+Yr4_wfpgrid.pdens = gsw_sigma0(Yr4_wfpgrid.S,Yr4_wfpgrid.T)+1000; 
+Yr4_wfpgrid.press = gsw_p_from_z(repmat(-Yr4_wfpgrid.depth_grid,length(Yr4_wfpgrid.profile_ind),1)',...
+        repmat(Yr4_wfpgrid.lat,1,length(Yr4_wfpgrid.depth_grid))'); 
+   
+    
 %Calculate O2 saturation    
     O2equil = gsw_O2sol_SP_pt(Yr1_wfpgrid.S,Yr1_wfpgrid.T);
 Yr1_wfpgrid.O2sat = (Yr1_wfpgrid.O2conc./O2equil - 1)*100;   
@@ -210,17 +276,22 @@ Yr1_wfpgrid.O2sat = (Yr1_wfpgrid.O2conc./O2equil - 1)*100;
 Yr2_wfpgrid.O2sat = (Yr2_wfpgrid.O2conc./O2equil - 1)*100;
     O2equil = gsw_O2sol_SP_pt(Yr3_wfpgrid.S,Yr3_wfpgrid.T);
 Yr3_wfpgrid.O2sat = (Yr3_wfpgrid.O2conc./O2equil - 1)*100;
+    O2equil = gsw_O2sol_SP_pt(Yr4_wfpgrid.S,Yr4_wfpgrid.T);
+Yr4_wfpgrid.O2sat = (Yr4_wfpgrid.O2conc./O2equil - 1)*100;
+
 %% If using separate up and down profiles, show comparison (don't do this if using profilePairMean above)
 %plotUpDownProfileComparisonWFP
 
 %% Visualize gridded data
-for i = 1:3
+for i = 1:4
     if i == 1
         plotting = Yr1_wfpgrid;
     elseif i == 2
         plotting = Yr2_wfpgrid;
     elseif i == 3
         plotting = Yr3_wfpgrid;
+    elseif i == 4
+        plotting = Yr4_wfpgrid;
     end
 
 
@@ -251,15 +322,15 @@ imagesc(plotting.chla); colorbar; caxis([0 0.3]); title('Chlorophyll a');
 end
 
 %% Merge all years of data into a single dataset (no corrections)
-wfpmerge.time = [Yr1_wfpgrid.time_start(Yr1_wfpgrid.ind_pair); Yr2_wfpgrid.time_start(Yr2_wfpgrid.ind_pair); Yr3_wfpgrid.time_start(Yr3_wfpgrid.ind_pair)];
+wfpmerge.time = [Yr1_wfpgrid.time_start(Yr1_wfpgrid.ind_pair); Yr2_wfpgrid.time_start(Yr2_wfpgrid.ind_pair); Yr3_wfpgrid.time_start(Yr3_wfpgrid.ind_pair); Yr4_wfpgrid.time_start(Yr4_wfpgrid.ind_pair)];
 wfpmerge.depth_grid = Yr1_wfpgrid.depth_grid;
-wfpmerge.T = [Yr1_wfpgrid.T Yr2_wfpgrid.T Yr3_wfpgrid.T];
-wfpmerge.S = [Yr1_wfpgrid.S Yr2_wfpgrid.S Yr3_wfpgrid.S];
-wfpmerge.pdens = [Yr1_wfpgrid.pdens Yr2_wfpgrid.pdens Yr3_wfpgrid.pdens];
-wfpmerge.O2conc = [Yr1_wfpgrid.O2conc Yr2_wfpgrid.O2conc Yr3_wfpgrid.O2conc];
-wfpmerge.O2sat = [Yr1_wfpgrid.O2sat Yr2_wfpgrid.O2sat Yr3_wfpgrid.O2sat];
-wfpmerge.backscatter = [Yr1_wfpgrid.backscatter Yr2_wfpgrid.backscatter Yr3_wfpgrid.backscatter];
-wfpmerge.chla = [Yr1_wfpgrid.chla Yr2_wfpgrid.chla Yr3_wfpgrid.chla];
+wfpmerge.T = [Yr1_wfpgrid.T Yr2_wfpgrid.T Yr3_wfpgrid.T Yr4_wfpgrid.T];
+wfpmerge.S = [Yr1_wfpgrid.S Yr2_wfpgrid.S Yr3_wfpgrid.S Yr4_wfpgrid.S];
+wfpmerge.pdens = [Yr1_wfpgrid.pdens Yr2_wfpgrid.pdens Yr3_wfpgrid.pdens Yr4_wfpgrid.pdens];
+wfpmerge.O2conc = [Yr1_wfpgrid.O2conc Yr2_wfpgrid.O2conc Yr3_wfpgrid.O2conc Yr4_wfpgrid.O2conc];
+wfpmerge.O2sat = [Yr1_wfpgrid.O2sat Yr2_wfpgrid.O2sat Yr3_wfpgrid.O2sat Yr4_wfpgrid.O2sat];
+wfpmerge.backscatter = [Yr1_wfpgrid.backscatter Yr2_wfpgrid.backscatter Yr3_wfpgrid.backscatter Yr4_wfpgrid.backscatter];
+wfpmerge.chla = [Yr1_wfpgrid.chla Yr2_wfpgrid.chla Yr3_wfpgrid.chla Yr4_wfpgrid.chla];
 
 %% Plot merged data
 %Adjustable parameters for plotting
@@ -277,36 +348,36 @@ cmin = 27.5; cmax = 27.8; %manually set min and max
     cvec = [cmin:(cmax-cmin)/cints:cmax];
 contourf(X,Y,wfpmerge.pdens - 1000,cvec,'linecolor','none'); hold on;
 axis([min(wfpmerge.time) max(wfpmerge.time) mindepth maxdepth]); caxis([cmin cmax]);
-colormap(C); set(gca,'YDir','reverse'); ylabel('Depth (m)'); hcb = colorbar; set(hcb,'location','eastoutside')
+colormap(C); set(gca,'YDir','reverse'); ylabel('Depth (m)', 'Fontsize', 10); hcb = colorbar; set(hcb,'location','eastoutside')
 datetick('x',2,'keeplimits');
-title('\sigma_\theta')
+title('\sigma_\theta', 'Fontsize', 12)
 
     subplot(412) %Temperature
 cmin = 2; cmax = 6; %manually set min and max
     cvec = [cmin:(cmax-cmin)/cints:cmax];
 contourf(X,Y,wfpmerge.T,cvec,'linecolor','none'); hold on;
 axis([min(wfpmerge.time) max(wfpmerge.time) mindepth maxdepth]); caxis([cmin cmax]);
-colormap(C); set(gca,'YDir','reverse'); ylabel('Depth (m)'); hcb = colorbar; set(hcb,'location','eastoutside')
+colormap(C); set(gca,'YDir','reverse'); ylabel('Depth (m)', 'Fontsize', 10); hcb = colorbar; set(hcb,'location','eastoutside')
 datetick('x',2,'keeplimits');
-title('Temperature (deg C)')
+title('Temperature (deg C)', 'Fontsize', 12)
 
     subplot(413) %Oxygen concentration
 cmin = 230; cmax = 320; %manually set min and max
     cvec = [cmin:(cmax-cmin)/cints:cmax];
 contourf(X,Y,wfpmerge.O2conc,cvec,'linecolor','none'); hold on;
 axis([min(wfpmerge.time) max(wfpmerge.time) mindepth maxdepth]); caxis([cmin cmax]);
-colormap(C); set(gca,'YDir','reverse'); ylabel('Depth (m)'); hcb = colorbar; set(hcb,'location','eastoutside')
+colormap(C); set(gca,'YDir','reverse'); ylabel('Depth (m)', 'Fontsize', 10); hcb = colorbar; set(hcb,'location','eastoutside')
 datetick('x',2,'keeplimits');
-title('Oxygen concentration')
+title('Oxygen concentration (?mol/L)', 'Fontsize', 12)
 
     subplot(414) %Oxygen saturation
 cmin = -25; cmax = 0; %manually set min and max
     cvec = [cmin:(cmax-cmin)/cints:cmax];
 contourf(X,Y,wfpmerge.O2sat,cvec,'linecolor','none'); hold on;
 axis([min(wfpmerge.time) max(wfpmerge.time) mindepth maxdepth]); caxis([cmin cmax]);
-colormap(C); set(gca,'YDir','reverse'); ylabel('Depth (m)'); hcb = colorbar; set(hcb,'location','eastoutside')
+colormap(C); set(gca,'YDir','reverse'); ylabel('Depth (m)', 'Fontsize', 10); hcb = colorbar; set(hcb,'location','eastoutside')
 datetick('x',2,'keeplimits');
-title('Oxygen saturation (%)')
+title('Oxygen saturation (%)', 'Fontsize', 12)
 
 figure (i + 2); clf;
     subplot(211) %Backscatter
@@ -325,5 +396,5 @@ contourf(X,Y,wfpmerge.chla,cvec,'linecolor','none'); hold on;
 axis([min(wfpmerge.time) max(wfpmerge.time) mindepth maxdepth]); caxis([cmin cmax]);
 colormap(C2); set(gca,'YDir','reverse'); ylabel('Depth (m)'); hcb = colorbar; set(hcb,'location','eastoutside')
 datetick('x',2,'keeplimits');
-title('Chlorophyll')
+title('Chlorophyll (µg/L)')
 
