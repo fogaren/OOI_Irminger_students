34894f97ef78b36ad74287e975732301f4f2b36b
Hilary Palevsky <hpalevsky@whoi.edu>
2018-11-30 21:06:20 -0500

    Added ability to specify desired depth for time-series plotting

--- Lucy/mooring_respirationrates.m
+++ Lucy/mooring_respirationrates.m
@@ -1,9 +1,12 @@
 %dissolved oxygen time series at certain depths to show respiration
 
-[X,Y] = meshgrid(wfpmerge.time, wfpmerge.depth_grid);
-Z = find(wfpmerge.oxygen_corr(11,:)>260);
+depth_to_plot = 200; %set depth to plot
+    [~, depth_id] = min(abs(depth_grid - depth_to_plot)); %find index in depth_grid corresponding to depth_to_plot
+minval = 270; %set minimum value to include in plot (to exclude low outliers)
 
-%D1 = find(wfpmerge.time == );
+%[X,Y] = meshgrid(wfpmerge.time, wfpmerge.depth_grid);
+Z = find(wfpmerge.oxygen_corr(depth_id,:) > minval);
+
 figure(1); clf;
-scatter(wfpmerge.time(Z),wfpmerge.oxygen_driftcorr(11,Z));
+scatter(wfpmerge.time(Z),wfpmerge.oxygen_driftcorr(depth_id,Z));
 datetick('x',2,'keeplimits');
