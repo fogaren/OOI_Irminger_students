b0a0d8c2b9debad01120ebe2c136d456c24a36fc
Lucy Wanzer <lwanzer@wellesley.edu>
2019-03-14 14:28:35 -0400

    didn't modify much, added a line that tried to only draw on lines for particular depths

--- Lucy/O2_Chl_w_redlines.m
+++ Lucy/O2_Chl_w_redlines.m
@@ -15,7 +15,8 @@ cmin = 260; cmax = 320; %manually set min and max
     cvec = [cmin:(cmax-cmin)/cints:cmax];
 contourf(X,Y,wfpmerge.oxygen_corr,cvec,'linecolor','none'); hold on;
 %contourf(X,Y,max_O2_season1,cvec,'r.'); hold on;
-plot(strat_beg_1_time(id_max_season1), wfpmerge.depth_grid, 'r.'); hold on; %want the y axis to be just the location of the max or min O2
+%plot(strat_beg_1_time(id_max_season1(1,15:1,231)), wfpmerge.depth_grid(1,15:1,231), 'r.'); hold on; %want the y axis to be just the location of the max or min O2
+plot(strat_beg_1_time(id_max_season1), wfpmerge.depth_grid, 'r.'); hold on;
 plot(strat_end_1_time(id_min_season1), wfpmerge.depth_grid, 'y.'); hold on;
 plot(strat_beg_2_time(id_max_season2), wfpmerge.depth_grid, 'r.'); hold on;
 plot(strat_end_2_time(id_min_season2), wfpmerge.depth_grid, 'y.'); hold on;
@@ -27,6 +28,25 @@ axis([min(wfpmerge.time) max(wfpmerge.time) mindepth maxdepth]); caxis([cmin cma
 colormap(C); set(gca,'YDir','reverse'); ylabel('Depth (m)', 'Fontsize', 10); hcb = colorbar; set(hcb,'location','eastoutside')
 datetick('x',2,'keeplimits');
 title('Oxygen concentration (mol/L)', 'Fontsize', 12)
+
+% figure(1);clf;
+% %subplot(311) %Oxygen_corr concentration
+% cmin = 260; cmax = 320; %manually set min and max
+%     cvec = [cmin:(cmax-cmin)/cints:cmax];
+% contourf(X,Y,wfpmerge.oxygen_corr,cvec,'linecolor','none'); hold on;
+% %contourf(X,Y,max_O2_season1,cvec,'r.'); hold on;
+% plot(strat_end_1_time(id_min_season1), wfpmerge.depth_grid, 'r.'); hold on; 
+% plot(strat_beg_1_time(id_max_season1), wfpmerge.depth_grid, 'y.'); hold on;
+% plot(strat_end_2_time(id_min_season2), wfpmerge.depth_grid, 'r.'); hold on;
+% plot(strat_beg_2_time(id_max_season2), wfpmerge.depth_grid, 'y.'); hold on;
+% plot(strat_end_3_time(id_min_season3), wfpmerge.depth_grid, 'r.'); hold on;
+% plot(strat_beg_3_time(id_max_season3), wfpmerge.depth_grid, 'y.'); hold on;
+% %plot(mindate_O2_season1, min_O2_season1, 'r.');
+% %scatter(maxdate_O2_season1, max_O2_season1,25,'r','*')
+% axis([min(wfpmerge.time) max(wfpmerge.time) mindepth maxdepth]); caxis([cmin cmax]);
+% colormap(C); set(gca,'YDir','reverse'); ylabel('Depth (m)', 'Fontsize', 10); hcb = colorbar; set(hcb,'location','eastoutside')
+% datetick('x',2,'keeplimits');
+% title('Oxygen concentration (mol/L)', 'Fontsize', 12)
 
 % subplot(312) %Oxygen saturation
 % cmin = -15; cmax = 0; %manually set min and max
